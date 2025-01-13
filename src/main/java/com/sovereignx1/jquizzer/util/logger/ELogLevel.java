af9bfdf971562a5117c44d8385c714da66585907
Michael Adolph <michael.w.adolph@gmail.com>
2025-01-12 22:58:12 -0500

    Fleshed out application context implementation, and set up logger debug configuration loading from app context (debug mode and log level)

--- src/main/java/com/sovereignx1/jquizzer/util/logger/ELogLevel.java
+++ src/main/java/com/sovereignx1/jquizzer/util/logger/ELogLevel.java
@@ -2,21 +2,20 @@ package com.sovereignx1.jquizzer.util.logger;
 
 enum ELogLevel {
 
-    VERBOSE("VERBOSE", 0),
-    DEBUG("DEBUG", 1),
-    INFO("INFO", 2),
-    WARN("WARN", 3),
+    VERBOSE("VERBOSE"),
+    DEBUG("DEBUG"),
+    INFO("INFO"),
+    WARN("WARN"),
 
     // Using the text ERROR causes intellij to capture them in a separate log. Console output will be normal, but
     // the ide will show an extra capture for errors using this log level. If you change the text to something
     // other than ERROR, it no longer works
-    ERROR("ERROR", 4);
+    ERROR("ERROR");
 
     private final String mStr;
-    private final int mLvl;
-    ELogLevel(String pStr, int mLvl){
+
+    ELogLevel(String pStr){
         mStr = pStr;
-        this.mLvl = mLvl;
     }
 
     @Override
