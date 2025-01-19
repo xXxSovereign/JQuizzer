2dce29cc67685bfac83859d00c15462df661ead1
Michael Adolph <michael.w.adolph@gmail.com>
2025-01-19 02:44:25 -0500

    Fixed a couple bugs
    - When the logger would close its resources, the log was not consistent with other logs. Updated to include timestamp.
    - There was an edge case that the LoggerImpl did not account for. Now when there are less values than
      braces, but at least 1 value, it will fill what braces it can and leaves the rest

--- src/main/java/com/sovereignx1/jquizzer/ui/mainmenu/MainMenuController.java
+++ src/main/java/com/sovereignx1/jquizzer/ui/mainmenu/MainMenuController.java
@@ -4,10 +4,16 @@ import com.sovereignx1.jquizzer.JQuizzerAppMain;
 import com.sovereignx1.jquizzer.util.logger.ILogger;
 import com.sovereignx1.jquizzer.util.logger.LoggerManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainMenuController {
 
@@ -16,13 +22,33 @@ public class MainMenuController {
 
     private final ILogger mLog = LoggerManager.getLogger();
 
+    private static final String NEW_DIALOG_FXML = "NewQuizzerDialog.fxml";
+
 
     @FXML
-    private void newDialog(){
-        mLog.info("executing substitution test {} {} {} ------", "LOL", "LMAO", "LMFAO");
-        mLog.info("executing new {} ------:", "test", "test23");
-        mLog.info("yoooo", "error lol", "extra values");
-        mLog.info("yooo {}");
+    private void newDialog() {
+        try {
+
+            mLog.info("executing substitution test {} {} {} ------", "LOL", "LMAO");
+            mLog.info("executing new {} ------:", "test", "test23");
+            mLog.info("yoooo", "error lol", "extra values");
+            mLog.info("yooo {}");
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(NEW_DIALOG_FXML));
+            Parent popupRoot = loader.load();
+
+            // Create the popup stage
+            Stage popupStage = new Stage();
+            popupStage.initModality(Modality.APPLICATION_MODAL); // Block interaction with other windows
+            popupStage.setTitle("Popup Window");
+
+            Scene popupScene = new Scene(popupRoot);
+            popupStage.setScene(popupScene);
+            popupStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -39,14 +65,17 @@ public class MainMenuController {
     }
 
     @FXML
-    private void exitDialog(){
+    private void exitDialog() {
         mLog.info("executing exit dialog");
+
+        // TODO: Overhaul this gay ass exit dialog with a custom FXML that does the same stuff. should be ez
 
         // Build small alert popup, and if confirmed exit application
         Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION);
         exitAlert.setTitle("Exit Confirmation");
         exitAlert.setHeaderText(null);
         exitAlert.setContentText("Do you really need to exit ?");
+
 
         ButtonType yesBtn = new ButtonType("Yes", ButtonBar.ButtonData.YES);
         ButtonType noBtn = new ButtonType("No", ButtonBar.ButtonData.NO);
